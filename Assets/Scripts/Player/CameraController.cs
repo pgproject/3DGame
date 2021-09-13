d1e70e740a0b9c6e0e759fa8d2cd99eb64f5331a
Grzegorz Pałka <grzp91@gmail.com>
2021-09-13 13:10:12 +0200

    Optymalization

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -6,30 +6,36 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    
     [SerializeField] private Camera m_camera;
-    [SerializeField] private float m_speedRotateHorizontal = 10f;
-    [SerializeField] private GameObject m_player;
-    private float m_xRotation;
 
+    private float m_pixelHight;
     private InputAccess m_inputAccess;
     private PlayerStats m_playerStats;
     private Vector3 m_mousePos;
-    [SerializeField]
     private float m_pitch;
+    private float m_verticalRotationSpeed;
+    private float m_xRotationClampMin;
+    private float m_xRotationClampMax;
+    private float m_cameraNearPlane;
     private void Awake()
     {
         m_inputAccess = GeneralScriptableObject.Instance.InputAccess;
         m_playerStats = GeneralScriptableObject.Instance.PlayerStats;
+        
+        m_verticalRotationSpeed = m_playerStats.SpeedVerticalRotation;
+        m_xRotationClampMin = m_playerStats.XRotationClampMin;
+        m_xRotationClampMax = m_playerStats.XRotationClampMax;
+        m_pixelHight = m_camera.pixelHeight / 2;
+        m_cameraNearPlane = m_camera.nearClipPlane;
     }
     void Update()
     {
         
         m_mousePos =
-            new Vector3(m_inputAccess.MousePositionX.reference.action.ReadValue<float>(), m_inputAccess.MousePositionY.reference.action.ReadValue<float>() - (m_camera.pixelHeight / 2), m_camera.nearClipPlane);
+            new Vector3(m_inputAccess.MousePositionX.reference.action.ReadValue<float>(), m_inputAccess.MousePositionY.reference.action.ReadValue<float>() - m_pixelHight, m_cameraNearPlane);
         
-        m_pitch -= m_mousePos.y * m_playerStats.SpeedVerticalRotation * Time.deltaTime;
-        m_pitch = Mathf.Clamp(m_pitch, m_playerStats.XRotationClampMin, m_playerStats.XRotationClampMax);
+        m_pitch -= m_mousePos.y * m_verticalRotationSpeed * Time.deltaTime;
+        m_pitch = Mathf.Clamp(m_pitch, m_xRotationClampMin, m_xRotationClampMax);
         
         transform.localRotation = Quaternion.Euler(m_pitch, 0, 0);
     }
